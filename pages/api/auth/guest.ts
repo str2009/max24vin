012396c6d84e959c61e55dbec4867bd460366a41
str2009 <suzo2009@gmail.com>
2025-06-07 18:12:52 +0300

    Update guest.ts

--- pages/api/auth/guest.ts
+++ pages/api/auth/guest.ts
@@ -1,21 +1,21 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { encode } from "next-auth/jwt";
-import { setCookie } from "nookies";
+import { NextApiRequest, NextApiResponse } from 'next';
+import { encode } from 'next-auth/jwt';
+import { setCookie } from 'nookies';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const name = req.query.name || "Guest";
+  const name = req.query.name || 'Guest';
 
   const token = await encode({
     token: { name, id: name },
     secret: process.env.NEXTAUTH_SECRET!,
   });
 
-  setCookie({ res }, "next-auth.session-token", token, {
+  setCookie({ res }, 'next-auth.session-token', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    path: "/",
-    maxAge: 60 * 60 * 24 * 7, // 7 дней
+    secure: process.env.NODE_ENV === 'production',
+    path: '/',
+    maxAge: 60 * 60 * 24 * 7,
   });
 
-  res.redirect("/");
+  res.redirect('/');
 }
